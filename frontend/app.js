98da68ce073e831a9294e0062e8a911f7beeabc4
KhalilRCG <khalilabebreseh@gmail.com>
2025-02-11 19:37:07 -0500

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,42 @@
+// Establish WebSocket connection for multiplayer chat
+let ws = new WebSocket("ws://localhost:8000/ws/player123");
+
+ws.onmessage = function(event) {
+    document.getElementById("chatOutput").innerText = event.data;
+};
+
+function sendMessage() {
+    let message = document.getElementById("chatInput").value;
+    ws.send(message);
+}
+
+// API Calls
+function generateCity() {
+    fetch("/city/build/player123", { method: "POST" })
+        .then(response => response.json())
+        .then(data => document.getElementById("cityOutput").innerText = data.city);
+}
+
+function generateQuest() {
+    fetch("/quest/player123", { method: "POST" })
+        .then(response => response.json())
+        .then(data => document.getElementById("questOutput").innerText = data.quest);
+}
+
+function generateHeist() {
+    fetch("/heist/generate/player123", { method: "POST" })
+        .then(response => response.json())
+        .then(data => document.getElementById("heistOutput").innerText = data.heist);
+}
+
+function tradeWeapons() {
+    fetch("/black-market/trade/weapons/10", { method: "POST" })
+        .then(response => response.json())
+        .then(data => document.getElementById("marketOutput").innerText = "Trade Value: " + data.value);
+}
+
+function startFactionWar() {
+    fetch("/factions/war/Red Talons/Obsidian Circuit", { method: "POST" })
+        .then(response => response.json())
+        .then(data => document.getElementById("warOutput").innerText = data);
+}
